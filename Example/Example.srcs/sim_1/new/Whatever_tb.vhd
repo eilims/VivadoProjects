92770cf2b36e03a68948f7e3375445942bbc6c30
eilims <dbenusovich@gmail.com>
2017-12-03 12:42:20 -0800

    asdf

---
+++ Example/Example.srcs/sim_1/new/Whatever_tb.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/18/2017 03:30:51 PM
+-- Design Name: 
+-- Module Name: Whatever_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Whatever_tb is
+--  Port ( );
+end Whatever_tb;
+
+architecture Behavioral of Whatever_tb is
+
+begin
+
+
+end Behavioral;
